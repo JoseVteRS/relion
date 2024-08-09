101874e897aad05fcdc44beca7c9194110ef04f5
Jose <jvrs.90@gmail.com>
2024-08-09 17:02:01 +0200

    add dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default async function RootLayout({
   const session = await auth();
   return (
     <SessionProvider session={session}>
-      <html lang="en" className="light">
+      <html lang="en" className="dark">
         <body className={`${inter.className}`}>
           <QueryProvider>
             <SheetProvider />
